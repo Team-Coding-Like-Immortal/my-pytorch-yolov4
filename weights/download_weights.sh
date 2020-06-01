98682329c866b71685d1bbaf1a87048ba484522e
whisperLiang <1851438730@qq.com>
2020-06-01 21:03:42 +0800

    update

---
+++ weights/download_weights.sh
@@ -0,0 +1,4 @@
+wget https://github.com/SOVLOOKUP/PyTorch-YOLOv4/releases/download/v1.0/yolov4.conv.137
+mkdir pre
+cd pre
+wget https://github.com/SOVLOOKUP/PyTorch-YOLOv4/releases/download/v1.0/yolov4.weights
